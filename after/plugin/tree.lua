dc5fb42decba8471a460e12f70592c97e6c272bb
LuisPg10 <dapin46@hotmail.com>
2024-02-08 22:05:44 -0500

    Changed git render option

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -2,7 +2,13 @@ local function my_on_attach(bufnr)
 	local api = require("nvim-tree.api")
 
 	local function opts(desc)
-		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+		return {
+			desc = "nvim-tree: " .. desc,
+			buffer = bufnr,
+			noremap = true,
+			silent = true,
+			nowait = true,
+		}
 	end
 
 	-- default mappings
@@ -10,7 +16,6 @@ local function my_on_attach(bufnr)
 
 	-- custom mappings
 	vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
-	vim.keymap.set("n", "<leader>h", api.tree.toggle_hidden_filter, opts("Show hidden files"))
 
 	-- deleted mappings
 	vim.keymap.del("n", "<C-t>", { buffer = bufnr })
@@ -19,6 +24,7 @@ end
 require("nvim-tree").setup({
 	on_attach = my_on_attach,
 	hijack_cursor = true,
+	select_prompts = true,
 	view = {
 		side = "right",
 		adaptive_size = true,
@@ -27,16 +33,16 @@ require("nvim-tree").setup({
 	},
 	renderer = {
 		root_folder_label = false,
-		highlight_git = true,
+		highlight_git = "all",
 		highlight_diagnostics = true,
 		indent_markers = {
 			enable = true,
 		},
 		icons = {
 			show = {
-				folder_arrow = false
-			}
-		}
+				folder_arrow = false,
+			},
+		},
 	},
 	filters = {
 		dotfiles = true,
